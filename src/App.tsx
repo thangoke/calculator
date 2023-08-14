a50114d567dfe3b5841e7fab0beee2cb6938b2cf
thangok <thangokenguyen@gmail.com>
2023-08-15 04:17:45 +0700

    define calculator keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,25 @@ function App() {
   return (
     <div className="App">
       <div style={styles.gridContainer}>
+        <Button>C</Button>
+        <Button>+/-</Button>
+        <Button>%</Button>
+        <Button>/</Button>
         <Button>1</Button>
         <Button>2</Button>
         <Button>3</Button>
+        <Button>X</Button>
         <Button>4</Button>
         <Button>5</Button>
         <Button>6</Button>
+        <Button>+</Button>
         <Button>7</Button>
         <Button>8</Button>
         <Button>9</Button>
+        <Button>-</Button>
+        <Button>.</Button>
+        <Button>0</Button>
+        <Button>=</Button>
       </div>
     </div>
   );
@@ -24,7 +34,7 @@ function App() {
 const styles: StyleSheet = {
   gridContainer: {
     display: "grid",
-    gridTemplateColumns: "auto auto auto",
+    gridTemplateColumns: "auto auto auto auto",
     backgroundColor: "#2196F3",
     padding: "10px",
   },
